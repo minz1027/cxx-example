296dbcb12b29367364e194256dc4791e94ed474e
Min Zhang <minzhangcmu@gmail.com>
2019-01-25 15:59:51 -0800

    fix: compile gtest

---
+++ install-gtest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get install cmake # install cmake
+cd /usr/src/gtest
+cmake CMakeLists.txt
+make
+
+# copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
+cp *.a /usr/lib
